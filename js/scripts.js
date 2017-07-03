c1b43691c9495aaeccd422e9fe0d5be2609df08a
Jesse Bryan and Robert Murray <jesse@>
2017-07-03 10:32:37 -0700

    initial commit for cooking unit converter

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var gallonsToLiters = function(gallon) {
+  return gallon * 3.78;
+}
+
+alert(gallonsToLiters(5));
+
+var ounceToGram = function(ounce) {
+  return ounce * 28.35;
+}
+
+alert(ounceToGram(8));
