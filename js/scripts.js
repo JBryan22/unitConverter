54e5f6aaf7510a979f0dfef8e3e8368c0bab7a73
Charlie Kelson and Jesse Bryan <jesse@>
2017-07-05 16:39:08 -0700

    create fields for users to input units to be converted.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,22 @@ var gallonsToLiters = function(gallon) {
   return gallon * 3.78;
 }
 
-alert(gallonsToLiters(5));
+var litersToGallons = function(liter){
+  return liter * 0.26;
+}
 
 var ounceToGram = function(ounce) {
   return ounce * 28.35;
 }
 
-alert(ounceToGram(8));
+$(function() {
+  $("#conversion").submit(function(event) {
+    event.preventDefault();
+    var gallons = $("#gallons").val();
+    var liters = $("#liters").val();
+    var convertedGallons = gallonsToLiters(gallons);
+    var convertedLiters = litersToGallons(liters);
+    $("#gallons").val(convertedGallons);
+    $("#liters").val(convertedLiters);
+  });
+});
